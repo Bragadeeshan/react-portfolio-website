2159a572aac7dfc5d7b3cae1dda11e7540f0e3b4
bragadeeshan1 <bragadeeshan@spadesapp.io>
2022-10-31 14:17:03 +0530

    Merge branch 'master'

--- src/Components/testimonials/Testimonials.jsx
+++ src/Components/testimonials/Testimonials.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './testimonial.css'
-import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR1 from '../../assets/avatar1.png'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
 
 // import Swiper core and required modules
 import {Pagination } from 'swiper';
@@ -20,32 +19,35 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar:AVTR1,
-    name:'Tina Snow',
-    review : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odio fugiat maxime explicabo facere tempora iusto voluptatibus ea qui. Natus eos quidem minus consequuntur aspernatur ad deleniti dolore asperiores maxime?'
+    name:'Dr. Isuru Nawinne',
+    position: 'Senior Lecturer',
+    address: 'Dept. of Computer Engineering Univerisity of Peradeniya',
+    email:'isurunawinne@eng.pdn.ac.lk',
+    phoneNo:'+9481 239 3470'
   },
   {
     avatar:AVTR2,
-    name:'Shatta Wale',
-    review : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odio fugiat maxime explicabo facere tempora iusto voluptatibus ea qui. Natus eos quidem minus consequuntur aspernatur ad deleniti dolore asperiores maxime?'
+    name:'Dr. Janaka Alawatugoda',
+    position: 'Senior Lecturer',
+    address: 'Dept. of Computer Engineering Univerisity of Peradeniya',
+    email:'alawatugoda@eng.pdn.ac.lk',
+    phoneNo:'+9481 239 3470'
   },
   {
     avatar:AVTR3,
-    name:'Kwase Despite',
-    review : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odio fugiat maxime explicabo facere tempora iusto voluptatibus ea qui. Natus eos quidem minus consequuntur aspernatur ad deleniti dolore asperiores maxime?'
-  },
-  {
-    avatar:AVTR4,
-    name:'Jon Snow',
-    review : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odio fugiat maxime explicabo facere tempora iusto voluptatibus ea qui. Natus eos quidem minus consequuntur aspernatur ad deleniti dolore asperiores maxime?'
+    name:'Dr. Damayanthi Herath',
+    position: 'Senior Lecturer',
+    address: 'Dept. of Computer Engineering Univerisity of Peradeniya',
+    email:'damayanthiherath@eng.pdn.ac.lk',
+    phoneNo:'+9481 239 3920'
   }
-
 ]
 
 const Testimonials = () => {
   return (
-    <section id='testimonial' >
+    <section id='testimonials' >
       <h5>Review From Clients</h5>
-      <h2>Testimonials</h2>
+      <h2>References</h2>
 
       <Swiper className="container testimonial__container"  
        modules={[Pagination]}
@@ -54,15 +56,17 @@ const Testimonials = () => {
        pagination={{ clickable: true }}
        >
        {
-        data.map(({avatar,name,review},index)=>{
+        data.map(({avatar,name,position,address,email,phoneNo},index)=>{
           return(
             <SwiperSlide key={index} className='testimonial'>
             <div className="client__avatar">
               <img src={avatar} alt="Avatar 1" />
             </div>
             <h5 className='client__name'> {name}</h5>
-            <small className='client__review'> {review}</small>
-  
+            <small className='client__review'> {position}</small>
+            <small className='client__review'> {address}</small>
+            <small className='client__review'> {email}</small>
+            <small className='client__review'> {phoneNo}</small>
           </SwiperSlide>
 
         )
